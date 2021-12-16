0c5a50eaa4353e9e2851f1dff61125f0ee97a622
Nyarlana <52347671+Nyarlana@users.noreply.github.com>
2021-12-16 16:28:04 +0100

    comments

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -23,7 +23,7 @@ public class Pathfinder : MonoBehaviour
     private Grid mapgrid;
     private static Vector2Int[] directions = { Vector2Int.down, Vector2Int.up, Vector2Int.right, Vector2Int.left };
 
-    // Start is called before the first frame update
+    // When awakened, get variables components
     void Awake()
     {
         mapgridhandler = maps.GetComponent<GridCollisionHandler>();
@@ -41,6 +41,15 @@ public class Pathfinder : MonoBehaviour
         } //same
     }
 
+    /*
+     * BFS Implementation
+     * Searching algorithm that scans a range in a grid in every direction equally. Used to make paths and scan terrain.
+     * V2Int, int, List<GameObject> -> Dictionary<V2Int, V2int>
+     * origin : starting point from which the search starts
+     * range : how far should the search go
+     * filter : list of gameobjects to filter from. This will edit the list to remove every GameObject not in search range from it.
+     * return parameter : map data dictionary. Key is an unique cell, value is its predecessor (from). Use map.Keys to get every cell in range, use in restorePath to generate a path.
+     */
     public Dictionary<Vector2Int, Vector2Int> BFS(Vector2Int origin, int range, List<GameObject> filter = null)
     {
         //setup
@@ -53,7 +62,7 @@ public class Pathfinder : MonoBehaviour
         Dictionary<Vector2Int, Vector2Int> pathdata = new Dictionary<Vector2Int, Vector2Int>();
         queue.Add(origin);
         pathdata.Add(origin, origin);
-        debug.SetTile(new Vector3Int(origin.x, origin.y, 0), queuedTile);
+        debug.SetTile(new Vector3Int(origin.x, origin.y, 0), queuedTile); //DEBUG
 
         //loop
         while (queue.Count > 0)
@@ -64,7 +73,7 @@ public class Pathfinder : MonoBehaviour
             foreach (Vector2Int point in queue_temp)
             {
                 explored.Add(point);
-                debug.SetTile(new Vector3Int(point.x, point.y, 0), exploredtile);
+                debug.SetTile(new Vector3Int(point.x, point.y, 0), exploredtile); //DEBUG
                 queue.Remove(point);
 
                 if (range > 0)
@@ -72,7 +81,7 @@ public class Pathfinder : MonoBehaviour
                 {
                     if (mapgridhandler.isTileWalkableLocal(direction + point) && !explored.Contains(direction + point) && !queue.Contains(direction + point))
                     {
-                        debug.SetTile(new Vector3Int(point.x+direction.x, point.y+direction.y, 0), queuedTile);
+                        debug.SetTile(new Vector3Int(point.x+direction.x, point.y+direction.y, 0), queuedTile); //DEBUG
                         queue.Add(direction + point);
                         pathdata.Add(point+direction, point);
                     }
@@ -94,8 +103,25 @@ public class Pathfinder : MonoBehaviour
         return pathdata;
     }
 
-    public List<Vector2Int> findPath(Vector2Int origin, Vector2Int arrival, int depth = 99) => restorePath(BFS(origin, depth), origin, arrival);
+    /*
+     * pathfinding simple access
+     * Creates a path from origin to arrival, or nothing if the path does not exist.
+     * V2int, V2Int, int -> List<V2Int>
+     * origin : starting point to find a path from
+     * arrival : ending point of the path
+     * depth : how far to search before giving up, defaults should be larger than map size.
+     * return value : series of positions in map space that forms a path.
+     */
+    public List<Vector2Int> findPath(Vector2Int origin, Vector2Int arrival, int depth = int.MaxValue) => restorePath(BFS(origin, depth), origin, arrival);
 
+    /*
+     * Generates a path from path data
+     * Dictionary<V2Int, V2Int>, V2Int, V2Int -> List<V2Int>
+     * map : pathdata (see BFS)
+     * origin : map coordinates of the starting point of the path
+     * target : map coordinates of the ending point of the path
+     * return value : path, in map space
+     */
     public List<Vector2Int> restorePath(Dictionary<Vector2Int, Vector2Int> map, Vector2Int origin, Vector2Int target)
     {
         List<Vector2Int> res = new List<Vector2Int>();
