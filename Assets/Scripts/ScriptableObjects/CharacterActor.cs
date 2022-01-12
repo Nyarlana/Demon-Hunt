4efe97edab3988684dc79882b9f843663b248dcc
darcnei <adrienfages@yahoo.com>
2022-01-12 12:30:07 +0100

    explication
    add new parameters in caracter
    explication and start with function in classe
    add scriptable object ennemi

--- Assets/Scripts/ScriptableObjects/CharacterActor.cs
+++ Assets/Scripts/ScriptableObjects/CharacterActor.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class CharacterActor : MonoBehaviour
 {
     public character classe;
-
+    private int attack;
+    private int health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attack = classe.attack;
+        health = classe.health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
+    }
+    void attackaction(CharacterActor hero, CharacterActor target)
+    {
+        target.health = target.health - hero.attack;
     }
 }
--- Assets/Scripts/ScriptableObjects/character.cs
+++ Assets/Scripts/ScriptableObjects/character.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Character", menuName = "Character")]//apperance in create
+[CreateAssetMenu(fileName = "New Character", menuName = "Character ")]//apperance in create 
 public class character : ScriptableObject
 {
     public new string name;
+    public string capacity1;
+    public string capacity2;
 
     public int attack;
     public int health;
+    public int speed;
+
+    public bool canHeal;
 
     public Sprite artwork;
 
-    public void capacity()//function for capacity
-    {
-
-    }
-    public void print() {
-        Debug.Log(name + " does " + attack + " damage and has " + health + " live ");
-      }
     
+    public void print() {
+        Debug.Log(name + " does " + attack + " damage with " + capacity1 + " and " + capacity2 + " in " + speed + " time and has " + health + " heath ");
+      }
+   
+    
+
 }
+
